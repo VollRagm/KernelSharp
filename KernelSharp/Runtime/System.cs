a953ac71db61ebf63c5034b4ea21da6afdcdf099
VollRagm <vollragm@gmail.com>
2021-12-18 17:23:56 +0100

    Added more imports to WDK.cs
    Added more imports and fixed build.bat

--- KernelSharp/Runtime/System.cs
+++ KernelSharp/Runtime/System.cs
@@ -25,6 +25,7 @@
     public struct UIntPtr { }
     public struct Single { }
     public struct Double { }
+    public struct Type { }
 
     public abstract class ValueType { }
     public abstract class Enum : ValueType { }
@@ -41,4 +42,73 @@
     public struct RuntimeFieldHandle { }
 
     public class Attribute { }
+
+    [AttributeUsage(AttributeTargets.Class, Inherited = true)]
+    public sealed class AttributeUsageAttribute : Attribute
+    {
+        private readonly AttributeTargets _attributeTarget;
+        private bool _allowMultiple;
+        private bool _inherited;
+
+        internal static readonly AttributeUsageAttribute Default = new AttributeUsageAttribute(AttributeTargets.All);
+
+        public AttributeUsageAttribute(AttributeTargets validOn)
+        {
+            _attributeTarget = validOn;
+            _inherited = true;
+        }
+
+        internal AttributeUsageAttribute(AttributeTargets validOn, bool allowMultiple, bool inherited)
+        {
+            _attributeTarget = validOn;
+            _allowMultiple = allowMultiple;
+            _inherited = inherited;
+        }
+
+        public AttributeTargets ValidOn => _attributeTarget;
+
+        public bool AllowMultiple
+        {
+            get => _allowMultiple;
+            set => _allowMultiple = value;
+        }
+
+        public bool Inherited
+        {
+            get => _inherited;
+            set => _inherited = value;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Enum, Inherited = false)]
+    public class FlagsAttribute : Attribute
+    {
+        public FlagsAttribute()
+        {
+        }
+    }
+
+    [Flags]
+    public enum AttributeTargets
+    {
+        Assembly = 0x0001,
+        Module = 0x0002,
+        Class = 0x0004,
+        Struct = 0x0008,
+        Enum = 0x0010,
+        Constructor = 0x0020,
+        Method = 0x0040,
+        Property = 0x0080,
+        Field = 0x0100,
+        Event = 0x0200,
+        Interface = 0x0400,
+        Parameter = 0x0800,
+        Delegate = 0x1000,
+        ReturnValue = 0x2000,
+        GenericParameter = 0x4000,
+
+        All = Assembly | Module | Class | Struct | Enum | Constructor |
+                        Method | Property | Field | Event | Interface | Parameter |
+                        Delegate | ReturnValue | GenericParameter
+    }
 }
--- KernelSharp/build.bat
+++ KernelSharp/build.bat
@@ -1,10 +1,8 @@
 @echo off
 
 :: you have to set the path to whatever it is for you
-@set ILCPATH = "E:\tools"
-
-SETLOCAL ENABLEEXTENSIONS
-
+@set ILCPATH="E:\tools"
+@set NTOSKRNLLIBPATH="D:\Windows Kits\10\lib\10.0.19041.0\km\x64\ntoskrnl.lib"
 
 @if not exist %ILCPATH%\ilc.exe (
   echo The DROPPATH environment variable not set.
@@ -19,11 +17,11 @@ SETLOCAL ENABLEEXTENSIONS
 
 @if "%1" == "clean" exit /B
 
-csc.exe /nologo /debug:embedded /noconfig /nostdlib /runtimemetadataversion:v4.0.30319 Program.cs WDK.cs Runtime/CompilerHelpers.cs Runtime/CompilerServices.cs Runtime/System.cs Runtime/Runtime.cs /out:Program.ilexe /langversion:latest /unsafe || goto Error
+csc.exe /nologo /debug:embedded /noconfig /nostdlib /runtimemetadataversion:v4.0.30319 Program.cs WDK.cs Runtime/InteropServices.cs Runtime/CompilerHelpers.cs Runtime/CompilerServices.cs Runtime/System.cs Runtime/Runtime.cs /out:Program.ilexe /langversion:latest /unsafe || goto Error
 %ILCPATH%\ilc Program.ilexe -o Program.obj --systemmodule Program --map Program.map -O || goto Error
 
 :: Also set this path to your oath
-link.exe "D:\Windows Kits\10\lib\10.0.19041.0\km\x64\ntoskrnl.lib" /nologo /subsystem:native /DRIVER:WDM Program.obj /entry:DriverEntry /incremental:no /out:Driver.sys || goto Error
+link.exe %NTOSKRNLLIBPATH% /nologo /subsystem:native /DRIVER:WDM Program.obj /entry:DriverEntry /incremental:no /out:Driver.sys || goto Error
 @goto :EOF
 
 :Error
